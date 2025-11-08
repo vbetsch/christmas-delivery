5a6c1623e21df5058ad56747ca853ff7dd876376
vbetsch <vic.betsch@gmail.com>
2025-11-08 16:59:25 +0100

    feat: initial commit based from mars-rover

---
+++ jest.config.cjs
@@ -0,0 +1,24 @@
+const { createDefaultPreset } = require('ts-jest');
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import('jest').Config} **/
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/src/app/**/*.test.ts'],
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+  },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
