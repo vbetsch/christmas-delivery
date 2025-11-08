cc6f616185f000096b1a6f7d7eb3c31b2ccf2658
vbetsch <vic.betsch@gmail.com>
2025-11-08 19:38:10 +0100

    fix: machine tests

--- src/domain/entities/machine/machine.test.ts
+++ src/domain/entities/machine/machine.test.ts
@@ -1,20 +1,40 @@
 import { MachineStatusEnum } from '@value-objects/statuses/machine-status.enum';
 import { Machine } from '@entities/machine/machine';
 import { Present } from '@entities/present/present';
+import { Logger } from '@value-objects/logger/logger';
 
 describe('Machine', () => {
+  let loggerSpeakSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    loggerSpeakSpy = jest.spyOn(Logger.prototype, 'speak');
+  });
+
+  afterEach(() => {
+    loggerSpeakSpy.mockRestore();
+  });
+
   it('should be well initialized', () => {
     const machine: Machine = new Machine();
     expect(machine).toBeDefined();
     expect(machine).toBeInstanceOf(Machine);
   });
+
   it('should be available', () => {
     const machine: Machine = new Machine();
     expect(machine.status).toStrictEqual(MachineStatusEnum.AVAILABLE);
   });
+
   it('should build a present', () => {
     const machine: Machine = new Machine();
     const present: Present = machine.buildPresent();
+
     expect(present).toBeInstanceOf(Present);
+    expect(loggerSpeakSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        className: 'Machine',
+        speech: 'A new present is built',
+      })
+    );
   });
 });
