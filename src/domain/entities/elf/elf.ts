65bc08cdef9c4002f2ad4f99324e00228e491161
vbetsch <vic.betsch@gmail.com>
2025-11-08 18:12:16 +0100

    feat: is loaded

--- src/domain/entities/elf/elf.ts
+++ src/domain/entities/elf/elf.ts
@@ -1,13 +1,20 @@
 import { ElfStatusEnum } from '@value-objects/elf-status.enum';
+import { Present } from '@entities/present/present';
 
 export class Elf {
   private readonly _status: ElfStatusEnum;
+  private readonly _presents: Present[];
 
   public constructor() {
     this._status = ElfStatusEnum.AVAILABLE;
+    this._presents = [];
   }
 
   public get status(): ElfStatusEnum {
     return this._status;
+  }
+
+  public isLoaded(): boolean {
+    return this._presents.length > 0;
   }
 }
