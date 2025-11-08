4a5f36c2fc937572a31ead5d8a5920ddfb15037d
vbetsch <vic.betsch@gmail.com>
2025-11-08 18:20:10 +0100

    feat: create e2e test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello world');
+import { Machine } from '@entities/machine/machine';
+import { Present } from '@entities/present/present';
+import { Elf } from '@entities/elf/elf';
+import { Sleigh } from '@entities/sleigh/sleigh';
+
+console.log('-------------- Christmas Delivery --------------');
+
+const toyMachine: Machine = new Machine();
+const deliveryElf: Elf = new Elf();
+const santaSleigh: Sleigh = new Sleigh();
+
+const currentPresent: Present = toyMachine.buildPresent();
+deliveryElf.takePresent(currentPresent);
+santaSleigh.loadPresent(currentPresent);
