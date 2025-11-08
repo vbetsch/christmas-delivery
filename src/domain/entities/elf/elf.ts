334a4544c6f0966a42a48e565eb0e2bd256117ce
vbetsch <vic.betsch@gmail.com>
2025-11-08 18:56:55 +0100

    refactor: create speak method

--- src/domain/entities/elf/elf.ts
+++ src/domain/entities/elf/elf.ts
@@ -3,10 +3,16 @@ import { Present } from '@entities/present/present';
 import { LoadPresentCommand } from '@entities/elf/commands/load-present.command';
 
 export class Elf {
+  private readonly _id: string;
   private readonly _status: ElfStatusEnum;
   private readonly _presents: Present[];
 
+  private _speak(speech: string): void {
+    console.log(`Elf #${this._id} - ${speech}`);
+  }
+
   public constructor() {
+    this._id = crypto.randomUUID();
     this._status = ElfStatusEnum.AVAILABLE;
     this._presents = [];
   }
@@ -24,10 +30,10 @@ export class Elf {
   }
 
   public async loadPresent(command: LoadPresentCommand): Promise<void> {
-    console.log('waiting...');
+    this._speak('waiting...');
     // eslint-disable-next-line @typescript-eslint/no-magic-numbers
     await new Promise((resolve) => setTimeout(resolve, 3000));
     command.sleigh.loadPresent(command.present);
-    console.log('done');
+    this._speak('done');
   }
 }
