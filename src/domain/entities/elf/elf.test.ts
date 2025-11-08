56d6cbb04c3c252e833ab6a339b06e55649cbefd
vbetsch <vic.betsch@gmail.com>
2025-11-08 18:10:20 +0100

    test: elf take present and is loaded

--- src/domain/entities/elf/elf.test.ts
+++ src/domain/entities/elf/elf.test.ts
@@ -1,11 +1,21 @@
 import { ElfStatusEnum } from '@value-objects/elf-status.enum';
 import { Elf } from '@entities/elf/elf';
+import { Present } from '@entities/present/present';
 
 describe('Elf', () => {
-  it('should have a status', () => {
+  it('should be well initialized', () => {
     const elf: Elf = new Elf();
     expect(elf).toBeDefined();
     expect(elf).toBeInstanceOf(Elf);
+  });
+  it('should be available and not be loaded', () => {
+    const elf: Elf = new Elf();
     expect(elf.status).toStrictEqual(ElfStatusEnum.AVAILABLE);
+    expect(elf.isLoaded()).toStrictEqual(false);
+  });
+  it('should take a present and be loaded', () => {
+    const elf: Elf = new Elf();
+    elf.takePresent(new Present());
+    expect(elf.isLoaded()).toStrictEqual(true);
   });
 });
